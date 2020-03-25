dcb3d09422a51d318fc4bd2a47bb9511422f8c7d
9moya <9moyapochta9@gmail.com>
2020-03-25 11:20:04 +0300

    3. Арифметические операции.[#260703]

--- src/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Vadim Kuznets");
-        System.out.println("09.08.1984");
-    }
-}
