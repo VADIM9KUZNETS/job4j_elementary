2e9ffe356188f553416908c8f67cfbb9e26cf9de
vadym.kuznets@yandex.ru <vadym.kuznets@yandex.ru>
2020-04-08 09:04:21 +0300

    6.4.1. Слово заканчивается на ...[#260672]--

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -4,11 +4,12 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int index = 0; index < post.length; index++) {
-            if (word[word.length - 1] != post[post.length - 1]) {
+            if (post[post.length - 1 - index] != word[word.length - 1 - index]) {
                 result = false;
+
             }
         }
-        // проверить. что массив word имеет последние элементы одинаковые с post
         return result;
+
     }
 }
